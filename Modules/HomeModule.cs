d88cb5f7effeda198a20d49cec6bf62db4c46097
Alan Denison <alan79td@gmail.com>
2016-07-01 10:02:02 -0700

    take a name and post it under contact created

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,15 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/new"] = _ => {
+        return View["/Contact_Form.cshtml"];
+      };
+      Post["/new-contact"] = _ => {
+        Contact newContact = new Contact(Request.Form["new-name"]);
+        List<Contact> allContacts = Contact.GetAll();
+        return View["Contact_Created.cshtml", newContact];
+      };
+
     }
   }
 }
